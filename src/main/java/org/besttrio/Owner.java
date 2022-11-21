2a5b5f6370d75b8f6f74a6e5995f140cbc05110e
tokrist <68947997+tokrist@users.noreply.github.com>
2022-11-21 10:45:12 +0100

    create Owner.java

--- src/main/java/org/besttrio/Owner.java
+++ src/main/java/org/besttrio/Owner.java
@@ -1,4 +1,24 @@
 package org.besttrio;
 
 public class Owner {
+    private int tulajdonosKod;
+    private String nev;
+    private String cim;
+
+    public Owner(int tulajdonosKod, String nev, String cim) {
+        this.tulajdonosKod = tulajdonosKod;
+        this.nev = nev;
+        this.cim = cim;
+    }
+
+    public int getTulajdonosKod() {
+        return tulajdonosKod;
+    }
+    public String getNev() {
+        return nev;
+    }
+    public String getCim() {
+        return cim;
+    }
+
 }
